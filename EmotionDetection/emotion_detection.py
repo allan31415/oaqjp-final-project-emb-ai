8dbf918a78c5a47e8eef4a0fc312afa1e2fce231
Allan31415 <allan31415@gmail.com>
2026-08-22 21:10:02 -0400

    Final

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,13 +6,25 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL, json = myobj, headers=headers)
-    formatted_response = json.loads(response.text)
-    #extract emotion scores
-    angersc = formatted_response['emotionPredictions'][0]['emotion']['anger']
-    disgustsc = formatted_response['emotionPredictions'][0]['emotion']['disgust']
-    fearsc = formatted_response['emotionPredictions'][0]['emotion']['fear']
-    joysc = formatted_response['emotionPredictions'][0]['emotion']['joy']
-    sadnesssc = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    status_code = response.status_code
+    if status_code == 200:
+        formatted_response = json.loads(response.text)
+        #extract emotion scores
+        angersc = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgustsc = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fearsc = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joysc = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadnesssc = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    elif status_code == 400:
+        angersc = "None"
+        disgustsc = "None"
+        fearsc = "None"
+        joysc = "None"
+        sadnesssc = "None"
+
+    else:
+        return "Something went wrong, please try again"
 
     #determine dominant emotion
     emotion_dict = {'anger': angersc, 
@@ -20,7 +32,10 @@ def emotion_detector(text_to_analyze):
                     'fear': fearsc,
                     'joy': joysc,
                     'sadness': sadnesssc}
-    dominant_emotion = max(emotion_dict, key=emotion_dict.get)
+    if status_code == 400:
+        dominant_emotion = "None"
+    else:
+        dominant_emotion = max(emotion_dict, key=emotion_dict.get)
     emotion_dict['dominant_emotion'] = dominant_emotion
     return emotion_dict
 
--- server.py
+++ server.py
@@ -1,24 +1,34 @@
+''' This is the server script which handles the web responses
+and decoration.
+'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route("/") 
-def render_index_page(): 
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
     return render_template('index.html')
 
 @app.route('/emotionDetector')
 def emotion_det():
+    ''' This function analyzes the response and formats it properly.
+    '''
     text_to_analyze = request.args.get('textToAnalyze')
     analyzed_text = emotion_detector(text_to_analyze)
     dominant_emotion = analyzed_text['dominant_emotion']
+    if dominant_emotion == "None":
+        return "Invalid text! Please try again!"
+
     items = list(analyzed_text.items())
     emotions = ", ".join([f"'{k}': {v}" for k, v in items[:-2]])
     last_emotion = f"'{items[-2][0]}': {items[-2][1]}"
     emotions_string = f"{emotions} and {last_emotion}"
-    return (f"For the given statement, the system response is {emotions_string}. The dominant emotion is {dominant_emotion}.")
+    return f"For the given statement, the system response is {emotions_string}. \
+        The dominant emotion is {dominant_emotion}."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
